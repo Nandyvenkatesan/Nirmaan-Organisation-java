56c1849c73feaa6c14fbb500f65d23541e15e750
Nandyvenkatesan <nandhuvenkat40@gmail.com>
2024-12-16 16:57:00 +0530

    Add files via upload

---
+++ variabletask.java
@@ -0,0 +1,18 @@
+class variabletask{
+public static void main(String args[]){
+byte j = 77;
+System.out.println("byte : " +j);
+int n=88;
+System.out.println("int : " +n);
+long s=9978925;
+System.out.println("long: "+s);
+float g=44.2f;
+System.out.println("float: "+g);
+String Name= "Nandhini";
+System.out.println("String: "+Name);
+char c = '2';
+System.out.println("char:"+c);
+}
+}
+
+
