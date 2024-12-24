de75c0d166177edbd46a8a9484fb55b48ac15a1f
Nandyvenkatesan <nandhuvenkat40@gmail.com>
2024-12-24 15:59:22 +0530

    day4

---
+++ LoopS.java
@@ -0,0 +1,46 @@
+package day4;
+
+import java.util.Scanner;
+
+public class LoopS {
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		/*for(int i=0; i<10; i++){
+		System.out.println(i);
+		
+		
+	}
+	}
+	//***************
+	
+	System.out.println("enter the num ");
+	int m = sc.nextInt() ;
+	for(int i=0; i<=10; i++) {
+		if(i%4==0) {
+			System.out.println("nandhini");
+			
+		}
+		else {
+			System.out.println(i);
+			
+		}
+	}*/
+		
+		//*********
+		
+		/*for(int i=0; i<=5; i++) {
+			for(int j=0; j<i; j++)
+			System.out.print(i);
+			
+		}
+
+		}*/
+		System.out.println("enter the num");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println(n+ "*"+i "=" +n*i);
+		}
+	
+
+}
+
