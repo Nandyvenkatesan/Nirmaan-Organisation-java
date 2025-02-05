28475d34b6e609a62d5e39129fcbff62ab1e6925
Nandyvenkatesan <nandhuvenkat40@gmail.com>
2025-02-05 16:10:48 +0530

    Day 20

---
+++ wrapperclass.java
@@ -0,0 +1,52 @@
+package Wrapper;
+
+public class wrapperclass {
+	public static void main(String[] args) {
+		int a=77;
+		Integer a1 = a;
+		int b = a1;
+		System.out.println(a1);
+		
+		char c = 'J';
+		Character b1=c;
+		char d = b1;
+		System.out.println(b1);
+		
+		byte e = 124;
+		Byte f =e;
+		byte w=f;
+		System.out.println(f);
+		
+		float g = 8;
+		Float h =g;
+		float k=h;
+		System.out.println(h);
+		
+		
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
